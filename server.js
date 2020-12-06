adf2d6fa74b260c9c1d5c455a6c20b08c5afdab3
Meyvi <meyvim@gmail.com>
2020-12-05 21:15:23 -0800

    Adds the remaining services and new url is localhost:3000/products/:id

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
+const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+app.use(express.static(path.join(__dirname,'../dist')));
+
+app.get('/products/:id', (req, res) => {
+  res.sendFile('/Users/Meyvi/Documents/Hack_Reactor/FEC/Meyvi-Proxy/public/index.html');
+});
 
 app.listen(3000, () => {
   console.log('server listening at 3000');

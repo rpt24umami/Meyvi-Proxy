2697bf095f78237e798dc827fa5dfd0732d0ed14
Meyvi <meyvim@gmail.com>
2020-12-06 11:48:50 -0800

    Styles the proxy html

--- server.js
+++ server.js
@@ -7,9 +7,27 @@ const port = 3000;
 app.use(cors());
 app.use(express.static(path.join(__dirname,'../dist')));
 
+// app.get('/products/:id', (req, res) => {
+//   res.sendFile('/Users/Meyvi/Documents/Hack_Reactor/FEC/Meyvi-Proxy/public/index.html');
+// });
+
 app.get('/products/:id', (req, res) => {
-  res.sendFile('/Users/Meyvi/Documents/Hack_Reactor/FEC/Meyvi-Proxy/public/index.html');
+  if (req.params.id === 'bundle.js') {
+    res.sendFile(path.resolve(__dirname, 'public/bundle.js'));
+  } else if (req.params.id === 'style.css') {
+    res.sendFile(path.resolve(__dirname, 'public/style.css'));
+  } else {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+  }
 });
+
+app.get('/Reviews.jpg', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/Reviews.jpg'));
+});
+
+app.get('/Cart.jpg', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/Cart.jpg'));
+})
 
 app.listen(3000, () => {
   console.log('server listening at 3000');
